9efbe89ea71da733c63fdd82df37855b7c49bb4e
Watahman <emile.vandewaetere@gmail.com>
2026-08-06 17:47:06 +0200

    fix(i18n): assemble French completion dictionary

---
+++ src/assets/js/i18n/locale-completions/fr.js
@@ -0,0 +1,5 @@
+import publicCopy from './fr-public.js';
+import guidance from './fr-guidance.js';
+import other from './fr-other.js';
+
+export default Object.freeze({ ...publicCopy, ...guidance, ...other });
