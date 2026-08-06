b9c5f1390cbd7bcb944f2949d50f899a16d1714f
Watahman <emile.vandewaetere@gmail.com>
2026-08-06 19:10:45 +0200

    aggregate additional policy translations

---
+++ src/assets/js/i18n/public-policy-extra-locales.js
@@ -0,0 +1,5 @@
+import fr from './public-policy-extra/fr.js';
+import de from './public-policy-extra/de.js';
+import es from './public-policy-extra/es.js';
+
+export const publicPolicyExtraLocales = Object.freeze({ fr, de, es });
