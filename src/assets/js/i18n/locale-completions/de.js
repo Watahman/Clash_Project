1f2f294c02f290c87e61e6b0c53c023568a34ef1
Watahman <emile.vandewaetere@gmail.com>
2026-08-06 17:47:14 +0200

    fix(i18n): assemble German completion dictionary

---
+++ src/assets/js/i18n/locale-completions/de.js
@@ -0,0 +1,5 @@
+import publicCopy from './de-public.js';
+import guidance from './de-guidance.js';
+import other from './de-other.js';
+
+export default Object.freeze({ ...publicCopy, ...guidance, ...other });
