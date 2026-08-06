d8e3bdae9ff6f6038b9b440d993628d2b34de795
Watahman <emile.vandewaetere@gmail.com>
2026-08-06 18:55:28 +0200

    aggregate guide translations

---
+++ src/assets/js/i18n/public-guides-locales.js
@@ -0,0 +1,7 @@
+import en from './public-guides/en.js';
+import nl from './public-guides/nl.js';
+import fr from './public-guides/fr.js';
+import de from './public-guides/de.js';
+import es from './public-guides/es.js';
+
+export const publicGuidesLocales = Object.freeze({ en, nl, fr, de, es });
