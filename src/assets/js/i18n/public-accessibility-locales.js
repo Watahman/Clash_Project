a5232b6a051b775cd3f2a108a869786737316a60
Watahman <emile.vandewaetere@gmail.com>
2026-08-06 19:18:10 +0200

    add fixed public accessibility labels

---
+++ src/assets/js/i18n/public-accessibility-locales.js
@@ -0,0 +1,46 @@
+const en = Object.freeze({
+    "homeV2.capabilitiesLabel": "ClashPanel capabilities",
+    "feature.about.capabilitiesLabel": "ClashPanel project characteristics",
+    "feature.planner.capabilitiesLabel": "Clash of Clans CWL Planner capabilities",
+    "feature.tracker.capabilitiesLabel": "Clash of Clans CWL Tracker capabilities",
+    "feature.family.capabilitiesLabel": "Clash of Clans Clan Family management capabilities",
+    "feature.bracket.capabilitiesLabel": "Clash of Clans Bracket Generator capabilities"
+});
+
+const nl = Object.freeze({
+    "homeV2.capabilitiesLabel": "Mogelijkheden van ClashPanel",
+    "feature.about.capabilitiesLabel": "Kenmerken van het ClashPanel-project",
+    "feature.planner.capabilitiesLabel": "Mogelijkheden van de Clash of Clans CWL Planner",
+    "feature.tracker.capabilitiesLabel": "Mogelijkheden van de Clash of Clans CWL Tracker",
+    "feature.family.capabilitiesLabel": "Mogelijkheden voor Clash of Clans Clan Family-beheer",
+    "feature.bracket.capabilitiesLabel": "Mogelijkheden van de Clash of Clans-bracketgenerator"
+});
+
+const fr = Object.freeze({
+    "homeV2.capabilitiesLabel": "Fonctionnalités de ClashPanel",
+    "feature.about.capabilitiesLabel": "Caractéristiques du projet ClashPanel",
+    "feature.planner.capabilitiesLabel": "Fonctionnalités du CWL Planner Clash of Clans",
+    "feature.tracker.capabilitiesLabel": "Fonctionnalités du CWL Tracker Clash of Clans",
+    "feature.family.capabilitiesLabel": "Fonctionnalités de gestion Clan Family de Clash of Clans",
+    "feature.bracket.capabilitiesLabel": "Fonctionnalités du générateur de bracket Clash of Clans"
+});
+
+const de = Object.freeze({
+    "homeV2.capabilitiesLabel": "Funktionen von ClashPanel",
+    "feature.about.capabilitiesLabel": "Eigenschaften des ClashPanel-Projekts",
+    "feature.planner.capabilitiesLabel": "Funktionen des Clash of Clans CWL Planners",
+    "feature.tracker.capabilitiesLabel": "Funktionen des Clash of Clans CWL Trackers",
+    "feature.family.capabilitiesLabel": "Funktionen der Clash of Clans Clan-Family-Verwaltung",
+    "feature.bracket.capabilitiesLabel": "Funktionen des Clash of Clans-Bracketgenerators"
+});
+
+const es = Object.freeze({
+    "homeV2.capabilitiesLabel": "Funciones de ClashPanel",
+    "feature.about.capabilitiesLabel": "Características del proyecto ClashPanel",
+    "feature.planner.capabilitiesLabel": "Funciones del CWL Planner de Clash of Clans",
+    "feature.tracker.capabilitiesLabel": "Funciones del CWL Tracker de Clash of Clans",
+    "feature.family.capabilitiesLabel": "Funciones de gestión de Clan Family de Clash of Clans",
+    "feature.bracket.capabilitiesLabel": "Funciones del generador de brackets de Clash of Clans"
+});
+
+export const publicAccessibilityLocales = Object.freeze({ en, nl, fr, de, es });
