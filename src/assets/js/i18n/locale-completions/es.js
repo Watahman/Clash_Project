405553a21a36f0849d25ae55419216213f9d2369
Watahman <emile.vandewaetere@gmail.com>
2026-08-06 17:47:22 +0200

    fix(i18n): assemble Spanish completion dictionary

---
+++ src/assets/js/i18n/locale-completions/es.js
@@ -0,0 +1,5 @@
+import publicCopy from './es-public.js';
+import guidance from './es-guidance.js';
+import other from './es-other.js';
+
+export default Object.freeze({ ...publicCopy, ...guidance, ...other });
