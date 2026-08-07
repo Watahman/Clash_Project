22cf8bf873f7a51bea108a4670c9839678af308e
Watahman <emile.vandewaetere@gmail.com>
2026-08-07 18:07:40 +0200

    feat: include battle achievement translations

--- src/assets/js/i18n/achievement-locales.js
+++ src/assets/js/i18n/achievement-locales.js
@@ -3,5 +3,19 @@ import en from './achievement-locales/en.js';
 import fr from './achievement-locales/fr.js';
 import de from './achievement-locales/de.js';
 import es from './achievement-locales/es.js';
+import { achievementBattleLocales } from './achievement-battle-locales.js';
 
-export const achievementLocales = Object.freeze({ nl, en, fr, de, es });
+function withBattleCopy(language, base) {
+    return Object.freeze({
+        ...base,
+        ...(achievementBattleLocales[language] || achievementBattleLocales.en)
+    });
+}
+
+export const achievementLocales = Object.freeze({
+    nl: withBattleCopy('nl', nl),
+    en: withBattleCopy('en', en),
+    fr: withBattleCopy('fr', fr),
+    de: withBattleCopy('de', de),
+    es: withBattleCopy('es', es)
+});
